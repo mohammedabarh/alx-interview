d9b27bc4c6951dfdaa5ee737d97d0dd7dec8c749
mohammedabarh <abmohammed606@gmail.com>
2025-05-02 00:34:49 +0000

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
-""" Minimum Operations
+"""
+Module that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
     """
+    Calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
 
+    Args:
+        n (int): The target number of H characters
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    Returns:
+        int: The minimum number of operations needed.
+             Returns 0 if n is impossible to achieve.
+    """
+    # If n is less than or equal to 1, return 0
+    if n <= 1:
         return 0
-    return op
+
+    operations = 0
+    divisor = 2
+
+    # Continue until n is fully factorized
+    while n > 1:
+        # If n is divisible by the current divisor
+        while n % divisor == 0:
+            # Add the divisor to operations
+            operations += divisor
+            # Divide n by the divisor
+            n = n // divisor
+        # Increment divisor to try next number
+        divisor += 1
+
+        # Optimization: if divisor squared is greater than n
+        # and n is greater than 1, then n is prime
+        if divisor * divisor > n and n > 1:
+            operations += n
+            break
+
+    return operations
