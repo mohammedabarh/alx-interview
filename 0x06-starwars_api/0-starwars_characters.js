c6e58e8187ae9b848a278e1860bc24d704fff6f6
mohammedabarh <abmohammed606@gmail.com>
2025-05-29 00:41:27 +0000

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,48 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2];
-if (!movieId) {
+// Check if movie ID is provided
+if (process.argv.length !== 3) {
+  console.error('Please provide a movie ID');
   process.exit(1);
 }
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
 
-request(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  if (res.statusCode !== 200) {
-    process.exit(1);
-  }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  function printCharacter(i) {
-    if (i >= characters.length) {
-      return;
-    }
-    request(characters[i], (error, response, charBody) => {
-      if (error) {
-        console.error(error);
-        process.exit(1);
-      }
-      if (response.statusCode !== 200) {
-        process.exit(1);
-      }
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-      printCharacter(i + 1);
+// Promisify request to handle async operations better
+const makeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, response, body) => {
+      if (error) reject(error);
+      else if (response.statusCode !== 200) {
+        reject(new Error(`Status Code: ${response.statusCode}`));
+      } else resolve(body);
     });
-  }
+  });
+};
 
-  printCharacter(0);
-});
+// Main async function to handle the character fetching
+async function getCharacters(movieId) {
+  try {
+    // Get movie data
+    const movie = await makeRequest(`${baseUrl}${movieId}`);
+    const characters = movie.characters;
+
+    // Fetch and print characters in order
+    for (const characterUrl of characters) {
+      try {
+        const character = await makeRequest(characterUrl);
+        console.log(character.name);
+      } catch (error) {
+        console.error(`Error fetching character: ${error.message}`);
+      }
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+// Execute the main function
+getCharacters(movieId);
